19e869552b77b41754a59c7dbe8c66686663eddb
Yuki Kida <kidayuki0628@gmail.com>
2021-08-11 14:20:31 +0900

    ユーザページの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :liked_imgposts, through: :likes, source: :imgpost
   has_many :comments, dependent: :destroy
+  validates :name, presence: true
+  validates :profile, length: {maximum: 200}
 
   def already_liked?(imgpost)
     self.likes.exists?(imgpost_id: imgpost.id)
