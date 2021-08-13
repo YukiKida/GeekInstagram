2a6b297a97503ba812936f19ef2956ebb92b9d00
Yuki Kida <kidayuki0628@gmail.com>
2021-08-13 19:36:28 +0900

    仮採点

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,29 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :liked_imgposts, through: :likes, source: :imgpost
   has_many :comments, dependent: :destroy
+  has_many :relationships
+  has_many :followings, through: :relationships, source: :follow
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
+  has_many :followers, through: :reverse_of_relationships, source: :user
   validates :name, presence: true
   validates :profile, length: {maximum: 200}
+
+  mount_uploader :image, ImageUploader
+
+  def follow(other_user)
+    unless self == other_user
+      self.relationships.find_or_create_by(follow_id: other_user.id)
+    end
+  end
+
+  def unfollow(other_user)
+    relationship = self.relationships.find_by(follow_id: other_user.id)
+    relationship.destroy if relationship
+  end
+
+  def following?(other_user)
+    self.followings.include?(other_user)
+  end
 
   def already_liked?(imgpost)
     self.likes.exists?(imgpost_id: imgpost.id)
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :users, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  resources :relationships, only: [:create, :destroy]
   get 'top/index' => 'top#index'
   resources :imgposts do
     resources :likes, only: [:create, :destroy]
